1a50113a2374efa72d89f328aced4df3546c46f8
Juniorrr912 <juniorlavado1988@gmail.com>
2026-03-09 11:59:07 -0300

    Add files via upload

---
+++ Programación/ejercicio 0.3.java
@@ -0,0 +1,12 @@
+ package mono;
+
+public class lol {
+
+	    public static void main(String[] args) {
+
+	        for (int i = 1; i <= 11; i += 2) {
+	            System.out.println(i + "\t" + (i + 1));
+	        }
+
+	    }
+	}
---
+++ Programación/ejercicio0.4.java
@@ -0,0 +1,12 @@
+package mono;
+
+public class lol{
+
+	    public static void main(String[] args) {
+
+	        for (int i = 1; i <= 6; i++) {
+	            System.out.println(i + "\t" + (i + 6));
+	        }
+
+	    }
+	}
